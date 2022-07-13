e4860221bb2193964e0120097a99fdeba5b00523
unknown <miraga.esgerli90@gmail.com>
2022-07-13 18:03:30 +0400

    new pages upload

---
+++ assets/js/currencyApi.js
@@ -0,0 +1,12 @@
+// const api = 'https://www.cbar.az/currencies/08.07.2022.xml';
+
+// function getData(api) {
+//     fetch(api)
+//     .then((res) => res.json())
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((err) => console.warn(err));
+// }
+
+// getData(api);
