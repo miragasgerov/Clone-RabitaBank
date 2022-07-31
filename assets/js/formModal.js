639a7e8dfa912126def46fe22f91e9b4632bd032
miragasgerov <miraga.esgerli90@gmail.com>
2022-07-31 13:35:11 +0400

    update Modal

--- assets/js/formModal.js
+++ assets/js/formModal.js
@@ -1,23 +1,41 @@
+
 var onlaynModal = document.querySelector(".onlaynModal");
 var davamBtn = document.getElementById("davamBtn");
 var closeBtn = document.querySelector(".close-btn");
+var loadingModal = document.querySelector(".loadingModal")
 
 // function toggleModal() {
 //     onlaynModal.classList.toggle("show-modal");
 // }
 
+
+
+
+
 davamBtn.addEventListener("click" , function(e) {
     e.preventDefault();
 
 
+    loadingModal.classList.toggle("show-modal");
+
+    setTimeout(modal,2000);
+
     onlaynModal.classList.toggle("show-modal");
+
+
 })
+
+function modal() {
+    loadingModal.style.display = "none";
+    onlaynModal.style.display = "block"
+}
 
 
 closeBtn.addEventListener("click" , (e) => {
     e.preventDefault();
 
     onlaynModal.classList.toggle("show-modal");
+
 
 })
 
